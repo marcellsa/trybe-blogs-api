0b9f29afc662fb8a45d8fa15fbb3de2b4f9eb6ce
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-02-15 17:22:58 -0300

    feat: req-07 - model Category criada

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const CategoryTable = sequelize.define('Category', { // Padrão: no singular
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+  },
+  {
+    tablename: 'categories', // Padrão: no plural
+    timestamps: false,
+  });
+  return CategoryTable;
+}
