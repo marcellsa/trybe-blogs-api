5be83f59431e770e4949c886284cb4aca45d785e
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-02-15 21:51:30 -0300

    fix: model Category e categoryService

--- src/models/Category.js
+++ src/models/Category.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
   {
     tablename: 'categories', // Padrão: no plural
     timestamps: false,
+    underscored: true,
   });
   return CategoryTable;
 }
